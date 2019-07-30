f8ef6d4a1275590bfe3dfb654e7ee0b244900528
Vitor Rios <vrios@users.noreply.github.com>
2019-07-30 15:27:07 -0300

    added analysis summarizing by parameter set

---
+++ analise.R
@@ -0,0 +1,33 @@
+
+library(here)
+
+library(dplyr)
+library(data.table) # fread is a faster file reader than read.table
+library(forcats)
+
+library(beepr)
+
+path <- file.path(here("output"))
+files <- list.files(path = path, full.names = T, pattern = ".csv") #só as primeiras 100 réplicas
+tmp <- lapply(files, data.table::fread, header = T, sep = ";")
+beep()
+save(tmp, file = "all.output.RData", compress = TRUE)
+#load("all.output.RData")
+species <- do.call(rbind, tmp)
+
+#sheep-plasticity;wolf-plasticity;cost-plasticity-sheep;cost-plasticity-wolf;sheep-gain-from-food;wolf-gain-from-food;wolf-gain-from-food;sheep-reproduce;wolf-reproduce;grass-regrowth-time;sizeimp;replicate-number;Richness; Shannon; Evenness; AbRelPgre; AbRelPvio; AbRelPgray; AbRelPsky; AbRelSone; AbRelStwo; AbRelSthree; AbRelSfou; AbRelWone; AbRelWtwo; AbRelWthree; AbRelWfour
+
+species %>% 
+   group_by(sheep_plasticity,
+            wolf_plasticity,
+            cost_plasticity_sheep,
+            cost_plasticity_wolf,
+            sheep_gain_from_food,
+            wolf_gain_from_food,
+            wolf_gain_from_food,
+            sheep_reproduce,
+            wolf_reproduce,
+            grass_regrowth_time,
+            sizeimp) %>%
+   summarize(mean_Richness = mean(Richness , na.rm = TRUE),
+             sd_Richness = sd(Richness , na.rm = TRUE)) ->Richness_per_set
