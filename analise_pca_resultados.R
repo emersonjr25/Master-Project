e8ff5512ea76c6ab397cc9c017300ea846976508
Unknown <emersonjr25@hotmail.com>
2019-09-09 20:03:47 -0300

    Resultados, análise no R pronta, entre outros

--- analise_pca_resultados.R
+++ analise_pca_resultados.R
@@ -1,37 +1,41 @@
-#PCA Analysis - option 1
-install.packages("stats")
-install.packages("ggplot2")
+#Packages - install and carry
+#install.packages("stats")
+#install.packages("ggplot2")
+#install.packages("xlsx")
+#install.packages("FactorMineR")
+#install.packages("factoextra")
 library(stats)
 library(ggplot2)
-dados <- read.csv()
-view(data)
-pcaresults <- prcomp (dados, scale = TRUE)
+library(xlsx)
+library(FactoMineR)
+library(factoextra)
+
+#PCA Analysis - option 1
+setwd("C:/Users/emers/Dropbox/Codigos/MestradoEmerson-master/MestradoEmerson-master/MestradoEmerson/Resultado_dados/")
+getwd()
+dados <- read.csv("C:/Users/emers/Dropbox/Codigos/MestradoEmerson-master/MestradoEmerson-master/MestradoEmerson/Resultado_dados/Variando plasticidade 8 combinacoes/8GeneralPlasticityHighCostHighPerturbationHighfractality.csv", header = TRUE, sep = ";", quote = "\"", dec = ",")
+#View(dados)
+pcaresults <- prcomp (dados[,12:24], scale = TRUE)
 pcaresults
-screenplot(pcaresults)
+screeplot(pcaresults)
 plot(pcaresults)
 summary(pcaresults)
 biplot(pcaresults)
-ggplot(data = pcaresults, aes())
-
-#library(FactorMineR)
-#PCA(dados)
 
 #PCA Analysis - option 2
-library(xlsx)
-library(FactorMineR)
-library(factoextra)
-
 pcaresults2 <- na.omit(dados)
-pcasubresults2 <- pcaresults2(1:12, 3:15)
-view(pcasubresults2)
-pcaresults2 <- scale(pcasubresults2)
-finalresultspca2 <- PCA(pcaresults2, graph = F)
+pcaresultsomit2 <- pcaresults2[, 12:24]
+#View(pcaresultsomit2)
+pcaresultsomit2 <- scale(pcaresultsomit2)
+#View(pcaresultsomit2)
+finalresultspca2 <- PCA(pcaresultsomit2, graph = F)
 eig.val <- get_eigenvalue(finalresultspca2)
 eig.val
 fviz_eig(finalresultspca2, addlabels = T, ylin = c(0,90))
 var <- get_pca_var(finalresultspca2)
 ind <- get_pca_ind(finalresultspca2)
 fviz_pca_var(finalresultspca2, col.var = "blue")
-grupo <- as.factor(dados[,1])
+grupo <- as.factor(dados[,2])
 fviz_pca_biplot(finalresultspca2, habillage = grupo, title = "Gráfico PCA")
+
 
